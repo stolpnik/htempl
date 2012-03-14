58b25a0e54ce8bc38d56f031179ae61da5037abe
mitsuhiro <iwasa@stolpnik.com>
2012-03-14 10:53:52 +0900

    modify

--- js/index.js
+++ js/index.js
@@ -5,3 +5,17 @@
  * Time: 13:26
  * To change this template use File | Settings | File Templates.
  */
+
+goog.require("goog.dom");
+goog.require('goog.array');
+goog.require('goog.events');
+
+var octo;
+
+/* initialize application */
+function initializeApp(){
+
+}
+
+goog.exportSymbol( "initializeApp", initializeApp );
+
